0d8fc888572fd6de411fd6f61a8e45f24e044fff
Chen Xie <is.chenx@gmail.com>
2021-05-08 17:05:42 -0700

    Finished smallest_nonconstructible_value

--- epi_judge_python/smallest_nonconstructible_value.py
+++ epi_judge_python/smallest_nonconstructible_value.py
@@ -3,12 +3,36 @@ from typing import List
 from test_framework import generic_test
 
 
+def seek_next_ind(nums, i):
+    original = nums[i]
+    while i < len(nums) and nums[i] == original:
+        i += 1
+    return i
+
+
 def smallest_nonconstructible_value(A: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    A.sort()
+    if A[0] != 1:
+        return 1
+
+    i = 0
+    cumsum = 0
+
+    while i < len(A):
+        if A[i] - cumsum > 1:
+            return cumsum + 1
+
+        j = seek_next_ind(A, i)
+        count = j - i
+        cumsum += count * A[i]
+        i = j
+
+    # print(cumsum)
+    return cumsum + 1
 
 
 if __name__ == '__main__':
+    # print(smallest_nonconstructible_value([1, 2, 3, 4]))
     exit(
         generic_test.generic_test_main('smallest_nonconstructible_value.py',
                                        'smallest_nonconstructible_value.tsv',
--- epi_judge_python/string_transformability.py
+++ epi_judge_python/string_transformability.py
@@ -1,11 +1,76 @@
+import collections
 from typing import Set
 
 from test_framework import generic_test
 
 
-def transform_string(D: Set[str], s: str, t: str) -> int:
-    # TODO - you fill in here.
-    return 0
+def differ_by_one(a, b):
+    """
+    Returns where a and b only differ by at most one char.
+
+    Shouldn't be called with same strings.
+    """
+    n_diff = 0
+
+    for i in range(len(a)):
+        if a[i] != b[i]:
+            n_diff += 1
+            if n_diff > 1:
+                return False
+
+    return True
+
+
+def transform_string(s_pool: Set[str], s0: str, s1: str) -> int:
+    """
+    s0, s1 and s_pool items all have the same length.
+
+    Approach: Make a graph, where nodes are strings, and edges connect
+    strings that only differ by one.
+
+    Complexity = O(v^2 + v).
+
+    Works but kind of slow.
+    """
+    # Make adjacency dict, where key is string, val[] contains edges to other
+    # strings.
+
+    # Need this for indexing strings to create edges between vertices.
+    all_strings = list(s_pool) + [s0, s1]
+    adj = {s: [] for s in all_strings}
+
+    for i in range(len(all_strings)):
+        for j in range(i, len(all_strings)):
+            # Careful with var names
+            a, b = all_strings[i], all_strings[j]
+            if differ_by_one(a, b):
+                adj[a].append(b)
+                adj[b].append(a)
+
+    # Use BFS to find shortest path if any.
+    # Records final distances. Also doubles as 'visited' array.
+    distances = {k: -1 for k in all_strings}  # [-1] * len(all_strings)
+    distances[s0] = 0
+
+    dist_from_start = 1
+    explore_verts = adj[s0]
+
+    while distances[s1] == -1 and explore_verts:
+        next_explore_verts = []
+
+        for u in explore_verts:
+            distances[u] = dist_from_start
+
+        for u in explore_verts:
+            # Visit neighbors that haven't been visited
+            for v in adj[u]:
+                if distances[v] == -1:
+                    next_explore_verts.append(v)
+
+        explore_verts = next_explore_verts
+        dist_from_start += 1
+
+    return distances[s1]
 
 
 if __name__ == '__main__':
